0cd4f741b352d463f52c7e05dda4dd937b37fd38
Ankit-Prajapati-it <jadavsubhu04@outlook.com>
2025-07-07 22:37:14 +0530

    complete till show todo list item

---
+++ React/src/components/projects/TO-DO/ToDo.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import "./ToDo.css";
+import { MdCheckCircle ,MdDeleteForever} from "react-icons/md";
+
+
+export const ToDo = () => {
+    const [inputValue,setInputValue] = useState("");
+    const [task,setTask] = useState([]);
+
+    const handleInputChange  = (value) =>{
+        setInputValue(value);
+    }
+
+    const handleFormSubmit = (event) =>{
+        event.preventDefault();
+
+    if (!inputValue) return;
+
+    if(task.includes(inputValue)){
+        setInputValue("");
+         return;
+    }
+        setTask((prevTask)=>[...prevTask,inputValue ]);
+    
+    setInputValue("");    
+    };
+    
+    return(
+        <section className="todo-container">
+            <header>
+                <h1>Todo List</h1>
+            </header>
+            <section className="form">
+                <form onSubmit={handleFormSubmit}>
+                    <div>
+                        <input
+                         type="text"
+                         className="todo-input"
+                         autoComplete="off"
+                         value={inputValue}
+                         onChange={(event)=>handleInputChange(event.target.value)}/>
+                    </div>
+                    <div>
+                        <button type="submit" className="todo-btn" onChange={handleFormSubmit}>Add Task </button>
+                    </div>
+                </form>
+            </section>
+            <section className="myUnOrdList">
+                <ul>
+                    {
+                        task.map((curTask,index)=>{
+                            return <li key={index} className="todo-item">
+                                <span>{curTask}</span>
+                                <button className="check-btn">
+                                    <MdCheckCircle  />
+                                </button>
+                                <button className="delete-btn">
+                                    <MdDeleteForever />
+                                </button>
+                            </li>                      
+                          })
+                    }
+                </ul>
+            </section>
+        </section>
+    )
+};

86bf8d36f1cfbb1b356f18e21ad02b2daef2833f
Ankit-Prajapati-it <jadavsubhu04@outlook.com>
2025-07-07 22:38:07 +0530

    toggleswitch

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,18 +1,25 @@
 // import "./components/Netflix.css"
 // import './components/Netflix.module.css';
 // import Netflix from "./components/Netflix";
-import { State } from './components/hooks/State';
-import "./components/EV.css";
-import { DerivedState } from './components/DerivedState';
+// import { State } from './components/hooks/State';
+// import "./components/EV.css";
+// import { DerivedState } from './components/DerivedState';
+// import { LiftingState } from './components/LiftStateUp';
+//  import { ToggleSwitch } from "./components/projects/ToggleSwitch/ToggleSwitch";
+
+import { ToDo } from "./components/projects/TO-DO/ToDo";
 
 
 export const App = () => {
   return (
-    <section className="container">
+    <section >
       {/* <h1 className="card-heading">List of Best Neetflix Series</h1> */}
       {/* <Netflix /> */}
       {/* <State/> */}
       {/* <DerivedState/> */}
+      {/* <LiftingState /> */}
+      {/* <ToggleSwitch /> */}
+      <ToDo/>
     </section>
   );
 };
